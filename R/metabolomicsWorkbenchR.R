0ddbb8e57383abd24ee3b3b40ce4ee82a92a2c6d
Gavin Rhys Lloyd <g.r.lloyd@bham.ac.uk>
2020-08-25 10:58:33 +0100

    update description and add see_also
    see_also links to API documentation, package vignette and do_query

--- R/metabolomicsWorkbenchR.R
+++ R/metabolomicsWorkbenchR.R
@@ -1,12 +1,19 @@
 #' metabolomicsWorkbenchR
 #'
 #' This package provides an interface to the Metabolomics Workbench API. It can 
-#' be used to query the database for study, compound, protein, gene information
-#' and more. All endpoints of the API are availble via simple methods. A number 
-#' of convenience functions are included e.g. to import study data as 
-#' SummarizedExperiment objects to facilite use within R and the Bioconductor 
-#' community.
+#' be used to query the Metabolomics Workbench for study, compound, protein, gene 
+#' information and more. All endpoints of the API are available via a simple 
+#' \code{do_query} method. A number of convenience functions are included to 
+#' import study data as \code{SummarizedExperiment} objects to facilitate use within R 
+#' and the Bioconductor community.
 #' @docType package
 #' @name metabolomicsWorkbenchR
+#' @seealso 
+#' \itemize{
+#'  \item \code{\link{do_query}} for a simple example.
+#'  \item \code{browseVignettes("metabolomicsWorkbenchR")} for more info.
+#'  \item Visit \url{https://www.metabolomicsworkbench.org/tools/mw_rest.php} for details of the API.
+#' }
+#' 
 NULL
 ## NULL
